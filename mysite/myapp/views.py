605bcbb978c931229829794291ce3f45d7efb7f0
WUBing <wubing@WUBingdeMBP.wv.cc.cmu.edu>
2017-11-09 10:12:16 -0500

    register

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -27,7 +27,8 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            # return redirect('home')
+            return HttpResponseRedirect('/login/')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
