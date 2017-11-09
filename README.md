505c657dcf2a808e96a028df79ad0ea50ad962af
WUBing <wubing@WUBingdeMBP.wv.cc.cmu.edu>
2017-11-09 15:45:52 -0500

    copy from master

---
+++ README.md
@@ -0,0 +1,4 @@
+# Task5
+
+## homepage
+
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from myapp import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': 'logged_out'}),
+    url(r'^logged_out/$', views.logout_result, name='logged_out'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^accounts/profile/$', views.account, name='account'),
+    url(r'^account/' , include('account.urls'))
+]
