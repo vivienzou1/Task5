2781e767cac353d33db7f7b6f60cbe804661b26e
WUBing <wubing@WUBingdeMacBook-Pro.local>
2017-11-10 12:50:29 -0500

    registration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'registration',
     'account',
 ]
 
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/confirmed/'
+LOGIN_URL = '/login/'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
-from myapp import views
-from django.contrib.auth import views as auth_views
+from registration import views
 
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^', include('myapp.urls')),
-    url(r'^account/' , include('account.urls'))
+    url(r'^', include('registration.urls')),
+    #url(r'^account/' , include('account.urls'))
 ]
