c24a893ddd8f5d6c36bc79e5f58fae2577b449da
WUBing <wubing@WUBingdeMBP.wv.cc.cmu.edu>
2017-11-09 19:39:52 -0500

    profile

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,10 +5,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, {'next_page': 'logged_out'}),
-    url(r'^logged_out/$', views.logout_result, name='logged_out'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^accounts/profile/$', views.account, name='account'),
+    url(r'^', include('myapp.urls')),
     url(r'^account/' , include('account.urls'))
 ]
