8530bc6716c80c40ab058183a9a70f51ead67344
WUBing <wubing@WUBingdeMBP.wv.cc.cmu.edu>
2017-11-11 15:23:07 -0500

    simulate account

--- mysite/log/urls.py
+++ mysite/log/urls.py
@@ -4,13 +4,11 @@ from account import views as account_views
 
 
 urlpatterns = [
-    url(r'^test_external/$', views.test_external, name="test_external"),
-    url(r'^test_internal/$', views.test_internal, name="test_internal"),
-    url(r'^external/(?P<user_name>.*)', views.get_log_external, name="get_log_external"),
-    url(r'^internal/(?P<user_name>.*)', views.get_log_internal, name="get_log_internal"),
-    url(r'^show/$', views.show_logs, name="show_logs"),
     url(r'^create/$', views.test_create, name="create_account"),
     url(r'^transfer/$', views.test_transfer, name="transfer"),
     url(r'^toSaving/$', views.test_check_to_saving, name="show_accounts"),
+    url(r'^external/(?P<user_name>.*)', views.get_log_external, name="get_log_external"),
+    url(r'^internal/(?P<user_name>.*)', views.get_log_internal, name="get_log_internal"),
+    url(r'^show/$', views.show_logs, name="show_logs"),
     url(r'^show_accounts/$', views.show_accounts, name="show_accounts"),
 ]
--- mysite/log/views.py
+++ mysite/log/views.py
@@ -9,27 +9,6 @@ from account import views as account_views
 from account.forms import *
 
 
-def test_external(request):
-    context = {}
-    if request.method == 'GET':
-        context['form'] = ExternalLogForm()
-        return render(request, 'test.html', context)
-    else:
-        form = ExternalLogForm(request.POST)
-        context['form'] = form
-        if not form.is_valid():
-            return render(request, 'test.html', context)
-        result = add_log_external(request,
-                                  form.cleaned_data['type'],
-                                  form.cleaned_data['amount'],
-                                  form.cleaned_data['account_number_1'],
-                                  form.cleaned_data['account_number_2'])
-        message = "log added"
-        context['message'] = message
-        context['result'] = result
-    return render(request, 'test.html', {'result': result})
-
-
 # show the user's account numbers (account, checking, saving)
 def show_accounts(request):
     context = {}
@@ -47,32 +26,14 @@ def show_logs(request):
     return render(request, 'log.html', {})
 
 
-def test_internal(request):
-    context = {}
-    if request.method == 'GET':
-        context['form'] = InternalLogForm()
-        return render(request, 'test.html', context)
-    else:
-        form = InternalLogForm(request.POST)
-        context['form'] = form
-        if not form.is_valid():
-            return render(request, 'test.html', context)
-        result = add_log_internal(request,
-                                  form.cleaned_data['type'],
-                                  form.cleaned_data['amount'],
-                                  request.user.username)
-        message = "log added"
-        context['message'] = message
-        context['result'] = result
-    return render(request, 'test.html', {'result': result})
-
-
+# simulate opening account
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['account_number']
 
 
+# simulate opening account
 def test_create(request):
     form = AccountForm
     if request.method == 'GET':
@@ -82,6 +43,7 @@ def test_create(request):
         return account_views.createAccount(request)
 
 
+# simulate transferring to others
 def test_transfer(request):
     form = TransferForm
     if request.method == 'GET':
@@ -90,23 +52,20 @@ def test_transfer(request):
         type = 'transfer'
         amount = request.POST['balance']
         user = request.user
-        print user.username
         account = Account.objects.get(user=user)
-        print account.account_number
         account_1 = Checking_Account.objects.get(account=account)
         account_number_1 = account_1.account_number
-        print account_number_1
         account_number_2 = request.POST['target_account']
-        print account_number_2
         context = add_log_external(request, type, amount, account_number_1, account_number_2)
-        print context
         return render(request, 'test_transfer.html', context)
 
 
+# simulate transfer from checking to saving
 class CheckToSavingForm(forms.Form):
     amount = forms.IntegerField()
 
 
+# simulate transfer from checking to saving
 def test_check_to_saving(request):
     form = CheckToSavingForm
     if request.method == 'GET':
@@ -119,6 +78,7 @@ def test_check_to_saving(request):
         return render(request, 'test_transfer.html', {'context': context})
 
 
+# get external log (transfer to others)
 @login_required
 def get_log_external(request, user_name):
     type = 'all'
@@ -127,34 +87,27 @@ def get_log_external(request, user_name):
     if request.method == "POST":
         try:
             user = get_object_or_404(User, username=user_name)
-            print user.username
             account = Account.objects.get(user=user)
-            print account.account_number
             checking_account = Checking_Account.objects.get(account=account)
-            print checking_account.account_number
-            print LogExternal.objects.filter(account_1=checking_account) | LogExternal.objects.filter(account_2=checking_account)
-            logs = LogExternal.objects.filter(account_1=checking_account) | LogExternal.objects.filter(
-                account_2=checking_account)
-            print logs[0]
-            print logs[0].account_1.account_number
+            logs = LogExternal.objects.filter(account_1=checking_account) | LogExternal.objects.filter(account_2=checking_account)
             if type is "transfer":
                 logs = logs.filter(type="T")
-                print "t"
             if type is "deposit":
                 logs = logs.filter(type="D")
             if type is "withdraw":
                 logs = logs.filter(type="W")
             r = construct_json_external(request, logs)
             r = r.replace('\n', ' ').replace('\r', '')
-            return HttpResponse(r, content_type='application/json')
+            return render(request, 'log.html', {'context': r})
         except:
             errors.append("error")
             context['errors'] = errors
             pass
 
-    return HttpResponse(context, content_type='application/json')
+    return render(request, 'log.html', {'context': r})
 
 
+# get internal log (transfer between accounts)
 @login_required
 def get_log_internal(request, user_name):
     type = 'all'
@@ -169,33 +122,29 @@ def get_log_internal(request, user_name):
             if type is "toSaving":
                 logs = logs.filter(type="S")
             r = construct_json_internal(request, logs)
-            print r
             r = r.replace('\n', ' ').replace('\r', '')
-            return HttpResponse(r, content_type='application/json')
+            return render(request, 'log.html', {'context': r})
         except:
             errors.append("error")
             context['errors'] = errors
             pass
 
-    return HttpResponse(context, content_type='application/json')
+    return render(request, 'log.html', {'context': r})
 
 
+# add external log
 def add_log_external(request, type, amount, account_number_1, account_number_2):
     context = {}
     errors = []
     try:
         account_1 = Checking_Account.objects.get(account_number=account_number_1)
-        print "account1"
         account_2 = Checking_Account.objects.get(account_number=account_number_2)
-        print "account2"
         new_log_external = LogExternal(type=type,
                                        amount=amount,
                                        account_1=account_1,
                                        account_2=account_2,
                                        )
-        print "new"
         new_log_external.save()
-        print "save"
         success = True
     except ObjectDoesNotExist:
         errors.append("Object does not exist.")
@@ -206,6 +155,7 @@ def add_log_external(request, type, amount, account_number_1, account_number_2):
     return context
 
 
+# add internal log
 def add_log_internal(request, type, amount, user_name):
     context = {}
     errors = []
@@ -225,6 +175,7 @@ def add_log_internal(request, type, amount, user_name):
     return context
 
 
+# delete external log
 def delete_log_external(request, log_id):
     context = {}
     errors = []
@@ -242,6 +193,7 @@ def delete_log_external(request, log_id):
     return context
 
 
+# delete internal log
 def delete_log_internal(request, log_id):
     context = {}
     errors = []
@@ -259,16 +211,14 @@ def delete_log_internal(request, log_id):
     return context
 
 
+# construct external logs to json
 def construct_json_external(request, logs):
-    print "construct"
     r = '['
     for log in logs:
-        print log.account_1.account_number
         r = r + '{"pk": ' + str(log.id) + ', '
         r = r + '"type": "' + log.type + '", '
         r = r + '"amount": ' + str(log.amount) + ', '
         r = r + '"account_1": ' + str(log.account_1.account_number) + ', '
-        print log.account_2.account_number
         r = r + '"account_2": ' + str(log.account_2.account_number) + ', '
         r = r + '}, '
     if len(logs) != 0:
@@ -278,13 +228,13 @@ def construct_json_external(request, logs):
     return r
 
 
+# construct internal logs to json
 def construct_json_internal(request, logs):
     r = '['
     for log in logs:
         r = r + '{"pk": ' + str(log.id) + ', '
         r = r + '"type": "' + log.type + '", '
         r = r + '"amount": ' + str(log.amount) + ', '
-        r = r + '"user": "' + log.user.username + '", '
         r = r + '}, '
     if len(logs) != 0:
         r = r[0:len(r) - 2]
