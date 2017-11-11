f08ad76cd5ab987fa50694ec90cca8ef123ce99e
WUBing <wubing@WUBingdeMBP.wv.cc.cmu.edu>
2017-11-10 19:17:33 -0500

    internal

--- mysite/log/views.py
+++ mysite/log/views.py
@@ -3,34 +3,51 @@ from django.contrib.auth.decorators import login_required
 from models import *
 from account.models import Account, Checking_Account
 from django.http import HttpResponse
-from forms import LogForm
+from forms import *
 
 
-def test(request):
-    form = LogForm()
-    return render(request, 'test.html', {'form': form})
+# def test(request):
+#     # form = ExternalLogForm()
+#     form = InternalLogForm()
+#     return render(request, 'test.html', {'form': form})
 
 
 @login_required
-def get_log_other_people(request, user_name):
+def get_log_external(request, user_name):
     user = get_object_or_404(User, username=user_name)
     account = Account.objects.get(user=user)
     checking_account = Checking_Account.objects.get(account=account)
-    logs = LogOtherPeople.objects.filter(account_1=checking_account) | LogOtherPeople.objects.filter(account_2=checking_account)
-    r = construct_json(request, logs)
+    logs = LogExternal.objects.filter(account_1=checking_account) | LogExternal.objects.filter(account_2=checking_account)
+    r = construct_json_external(request, logs)
     r = r.replace('\n', ' ').replace('\r', '')
     return HttpResponse(r, content_type='application/json')
 
 
-def add_log_other_people(request, type, amount, account_1, account_2):
-    new_log_other_people = LogOtherPeople(type=type,
-                                          amount=amount,
-                                          account_1=account_1,
-                                          account_2=account_2,)
-    new_log_other_people.save()
+@login_required
+def get_log_internal(request, user_name):
+    user = get_object_or_404(User, username=user_name)
+    logs = LogInternal.objects.filter(user=user)
+    r = construct_json_internal(request, logs)
+    r = r.replace('\n', ' ').replace('\r', '')
+    return HttpResponse(r, content_type='application/json')
 
 
-def construct_json(request, logs):
+def add_log_external(request, type, amount, account_1, account_2):
+    new_log_external = LogExternal(type=type,
+                                   amount=amount,
+                                   account_1=account_1,
+                                   account_2=account_2,)
+    new_log_external.save()
+
+
+def add_log_internal(request, type, amount, user):
+    new_log_internal = LogInternal(type=type,
+                                   amount=amount,
+                                   user=user)
+    new_log_internal.save()
+
+
+def construct_json_external(request, logs):
     r = '['
     for log in logs:
         r = r + '{"pk": ' + str(log.id) + ', '
@@ -38,6 +55,21 @@ def construct_json(request, logs):
         r = r + '"amount": "' + log.amount + '", '
         r = r + '"account_1": "' + log.account_1 + '", '
         r = r + '"account_2": "' + log.account_2 + '", '
+        r = r + '}, '
+    if len(logs) != 0:
+        r = r[0:len(r) - 2]
+    r = r + ']'
+    r = r[0: len(r) - 1] + ',{"username":"' + request.user.username + '"}]'
+    return r
+
+
+def construct_json_internal(request, logs):
+    r = '['
+    for log in logs:
+        r = r + '{"pk": ' + str(log.id) + ', '
+        r = r + '"type": "' + log.type + '", '
+        r = r + '"amount": "' + log.amount + '", '
+        r = r + '"user": "' + log.user + '", '
         r = r + '}, '
     if len(logs) != 0:
         r = r[0:len(r) - 2]
