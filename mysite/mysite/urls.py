4ae44b2e54e4903d00a96b7790c5e3c02bf133fe
LiLiu93 <ll2@andrew.cmu.edu>
2017-11-12 18:26:19 -0500

    all transaction function

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from registration import views
+from django.contrib import admin
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
     url(r'^', include('registration.urls')),
     url(r'^account/', include('account.urls'))
